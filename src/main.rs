977b4bde40ff7fbe836d2692937af81faf0fed1e
Ritam Sarmah <developer@ritam.me>
2025-07-14 09:14:08 -0700

    Add code review support
    (wip)

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,52 @@
-use anyhow::Result;
+use anyhow::{Result, bail};
 use reqwest::header::CONTENT_TYPE;
 use serde_json::{Value, json};
 use std::{
     env,
     io::{self, Read},
-    process::exit,
+    process::{Command, exit},
 };
+use termimad::{MadSkin, crossterm::style::Color};
 
-const SYSTEM_PROMPT: &str = "You are an expert pair programmer. Follow provided instructions and output minimal, idiomatic, raw code, without any Markdown formatting (no code fence backticks) or HTML tags. Keep any comments and style.";
-const BASE_URL: &str =
-    "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent";
+const BASE_URL: &str = "https://generativelanguage.googleapis.com/v1beta/models";
+const USAGE: &str = "Usage: pair <flag> <prompt>
+
+Flags:
+  -c, --code    Output code based on instructions
+  -r, --review  Review current changes in version control
+  -h, --help    Show this help message and exit";
 
 fn main() -> Result<()> {
-    let mut instructions = env::args()
-        .skip(1)
-        .collect::<Vec<_>>()
-        .join(" ")
-        .trim()
-        .to_owned();
+    let args: Vec<String> = env::args().collect();
 
-    if instructions.is_empty() {
-        instructions = "Finish the implementation and output the complete code".into();
+    if args.len() < 2 {
+        println!("{USAGE}");
+        exit(1);
     }
+
+    let flag = &args[1];
+    match flag.as_ref() {
+        "-c" | "--code" => {
+            let instructions = args
+                .get(2)
+                .cloned()
+                .unwrap_or("Finish the implementation and output the complete code".to_owned());
+
+            write_code(&instructions)?
+        }
+        "-r" | "--review" => review_changes()?,
+        "-h" | "--help" => println!("{USAGE}"),
+        _ => bail!("Unrecognized flag: {}", flag),
+    }
+
+    Ok(())
+}
+
+/// Writes code based on instructions to stdout
+fn write_code(instructions: &str) -> Result<()> {
+    let prompt = "You are an expert pair programmer.
+    Follow provided instructions and output minimal, idiomatic, raw code, without any Markdown formatting (no code fence backticks) or HTML tags.
+    Keep any comments and style.";
 
     let mut input = String::new();
     io::stdin().read_to_string(&mut input)?;
@@ -32,28 +57,68 @@ fn main() -> Result<()> {
         input.trim()
     );
 
+    let response = llm_response(prompt, &contents)?;
+    println!("{response}");
+
+    Ok(())
+}
+
+/// Reviews current changes in version control
+fn review_changes() -> Result<()> {
+    let output = Command::new("git").arg("diff").output()?.stdout;
+    let diff = String::from_utf8_lossy(&output);
+
+    if diff.is_empty() {
+        bail!("No code changes found")
+    }
+
+    let prompt = "You are an expert code reviewer.
+    Give a short, concise code review considering:
+    1. Bugs
+    2. Performance
+    3. Maintainability
+    4. Security
+    Only comment on issues; never validate good changes.
+    Always output Markdown, with a header for each section.
+    Never include surrounding code fence backticks.";
+
+    let response = llm_response(prompt, &diff)?;
+    let skin = get_markdown_skin();
+    skin.print_text(&response);
+
+    Ok(())
+}
+
+fn llm_response(prompt: &str, contents: &str) -> Result<String> {
+    let api_key = env::var("GEMINI_API_KEY")?;
     let body = json!({
-        "system_instruction": { "parts": [{ "text": SYSTEM_PROMPT }] },
+        "system_instruction": { "parts": [{ "text": prompt }] },
         "contents": [{ "parts": [{ "text": contents }] }],
         "generationConfig": { "thinkingConfig": { "thinkingBudget": 0 } }
     });
 
-    let api_key = env::var("GEMINI_API_KEY")?;
-
+    let url = format!("{BASE_URL}/gemini-2.5-flash:generateContent");
     let client = reqwest::blocking::Client::new();
     let response: Value = client
-        .post(BASE_URL)
+        .post(url)
         .header("x-goog-api-key", api_key)
         .header(CONTENT_TYPE, "application/json")
         .json(&body)
         .send()?
         .json()?;
 
-    if let Some(text) = response["candidates"][0]["content"]["parts"][0]["text"].as_str() {
-        println!("{text}");
-        Ok(())
-    } else {
-        eprintln!("Failed to extract response text");
-        exit(2);
-    }
+    let text = response["candidates"][0]["content"]["parts"][0]["text"].to_string();
+
+    Ok(serde_json::from_str(&text)?)
+}
+
+fn get_markdown_skin() -> MadSkin {
+    let mut skin = MadSkin::default();
+    skin.set_fg(Color::Reset);
+    skin.set_bg(Color::Reset);
+    skin.inline_code.set_fg(Color::White);
+    skin.inline_code.set_bg(Color::Black);
+    skin.code_block.set_fg(Color::Reset);
+    skin.code_block.set_bg(Color::Reset);
+    skin
 }
